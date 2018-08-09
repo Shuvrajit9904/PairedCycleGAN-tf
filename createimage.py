e749596f84f7abd2cb5cd81ddb79ca8ee581dec5
Shuvrajit Mukherjee <u1131529@utah.edu>
2018-08-09 00:30:38 -0600

    Makeups saved separately

--- createimage.py
+++ createimage.py
@@ -6,11 +6,12 @@ Created on Mon Jul 23 22:00:31 2018
 @author: shuvrajit
 """
 
-import os
+#import os
 import scipy.io as sio
 import pandas as pd
 import numpy as np
 from PIL import Image
+import re
 #from matplotlib import pyplot as plt
 
 
@@ -24,12 +25,19 @@ image_name_mat = {}
 
 df = pd.DataFrame(image_matrix)
 
+p_y = re.compile(".*_y.jpg")
+p_n = re.compile(".*_n.jpg")
+
+
 for column_idx in df:
     flat_data = np.asarray(df[column_idx])
     image_pixel = np.reshape(flat_data, (150, 130, 3), order = "F")
     image_name_mat[image_file_names[0][column_idx][0]] = image_pixel
     img = Image.fromarray(image_pixel, 'RGB')
-    img.save('data/YMU/images/'+image_file_names[0][column_idx][0])
+    if p_y.match(image_file_names[0][column_idx][0]):
+        img.save('data/YMU/images/makeup_y/'+image_file_names[0][column_idx][0])
+    if p_n.match(image_file_names[0][column_idx][0]):
+        img.save('data/YMU/images/makeup_n/'+image_file_names[0][column_idx][0])
     
     
 
